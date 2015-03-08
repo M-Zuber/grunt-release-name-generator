2a13a20fde0087f57198f5f6bca6572a8a27d3aa
Tim Ambler <tkambler@gmail.com>
2015-03-08 17:27:45 -0500

    Update release-name-generator.js

--- tasks/release-name-generator.js
+++ tasks/release-name-generator.js
@@ -1,16 +1,12 @@
 'use strict';
 
 var _ = require('lodash');
-_.mixin(require('underscore.string'));
 var util = require('util');
 var path = require('path');
 
 module.exports = function(grunt) {
 
-    var previousNamesSource = path.resolve('.grunt/grunt-release-name-generator/previous-names.json');
-
     var adjectives = require(__dirname + '/../lib/adjectives.json');
-
     var animals = {
         'birds': grunt.file.readJSON(__dirname + '/../lib/animals/birds.json'),
         'fish': grunt.file.readJSON(__dirname + '/../lib/animals/fish.json'),
@@ -18,84 +14,32 @@ module.exports = function(grunt) {
         'reptiles': grunt.file.readJSON(__dirname + '/../lib/animals/reptiles.json'),
         'mammals': grunt.file.readJSON(__dirname + '/../lib/animals/mammals.json')
     };
+    var categories = _.keys(animals);
 
-    var format = function(data) {
-        data = _.map(data, function(row) {
-            return _.capitalize(row.toLowerCase());
-        });
-        return data;
-    };
-
-    adjectives = format(adjectives);
-    _.each(animals, function(data, key) {
-        animals[key] = format(data);
-    });
-
-    var initDataDir = function() {
-        var srcFolder = path.resolve('.grunt/grunt-release-name-generator');
-        if (grunt.file.exists(srcFolder)) {
-            return;
-        }
-        grunt.file.mkdir(srcFolder);
-        grunt.file.write(srcFolder + '/previous-names.json', '[]');
-    };
-
-    var getPreviousNames = function() {
-        var previousNames = grunt.file.readJSON(previousNamesSource);
-        return previousNames;
-    };
-
-    var updatePreviousNames = function(name) {
-        var names = getPreviousNames();
-        names.push(name);
-        grunt.file.write(previousNamesSource, JSON.stringify(names));
-    };
-
-    var generateName = function(category, previousNames) {
-        initDataDir();
-        adjectives = _.shuffle(adjectives);
-        var adjective = adjectives[0];
-        var filteredAnimals = _.filter(animals[category], function(animal) {
+    var generateName = function(category) {
+        var adjective = _.shuffle(adjectives).pop();
+        var animal = _.filter(animals[category], function(animal) {
             if (animal[0] === adjective[0]) {
                 return true;
             }
-        });
-        if (!filteredAnimals.length) {
-            return generateName(category, previousNames);
+        }).pop();
+        if (!animal) {
+            return generateName(category);
         }
-        filteredAnimals = _.shuffle(filteredAnimals);
-        var name = adjective + ' ' + filteredAnimals[0];
-        if (previousNames.indexOf(name) >= 0) {
-            return generateName(category, previousNames);
-        }
-        updatePreviousNames(name);
-        return name;
+        return adjective + ' ' + animal;
     };
 
     grunt.registerMultiTask('release-name-generator', function() {
-
+        grunt.log.subhead('Generating release name for: ' + this.target);
         var options = this.options();
-        var categories = _.keys(animals);
-
         if (!options.category) {
-            options.category = _.shuffle(categories)[0];
+            options.category = _.shuffle(categories).pop();
         } else if (!animals[options.category]) {
             grunt.fail.fatal('Invalid category specified: ' + options.category);
         }
-
-        grunt.log.subhead('Generating release name for: ' + this.target);
-
-        var previousNames = getPreviousNames();
-
-        var name = generateName(options.category, previousNames);
-
+        var name = generateName(options.category);
         grunt.log.oklns('Release name: ' + name);
-
-        if (!global.release_name) {
-            global.release_name = {};
-        }
-        global.release_name[this.target] = name;
-
+        grunt.config('release-name-generator.' + this.target + '.name', name);
     });
 
 };
